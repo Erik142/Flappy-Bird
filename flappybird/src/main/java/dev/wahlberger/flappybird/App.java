c125a36595b7b201e29753470bc33fbdf1a9b390
Erik142 <EWahlberger@gmail.com>
2021-03-13 08:39:05 +0100

    Start game on startup

--- flappybird/src/main/java/dev/wahlberger/flappybird/App.java
+++ flappybird/src/main/java/dev/wahlberger/flappybird/App.java
@@ -1,4 +1,15 @@
-package dev.wahlberger;
+package dev.wahlberger.flappybird;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.awt.Point;
+
+import dev.wahlberger.flappybird.controller.GameKeyAction;
+import dev.wahlberger.flappybird.controller.MainMenuActionListener;
+import dev.wahlberger.flappybird.model.GameModel;
+import dev.wahlberger.flappybird.view.GamePanel;
+import dev.wahlberger.flappybird.view.MainMenuPanel;
+import dev.wahlberger.flappybird.view.MainWindow;
 
 /**
  * Hello world!
@@ -10,8 +21,27 @@ public final class App {
     /**
      * Says hello to the world.
      * @param args The arguments of the program.
+     * @throws URISyntaxException
      */
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main(String[] args) throws URISyntaxException, IOException {
+        GameModel gameModel = new GameModel();
+
+        MainWindow mainWindow = new MainWindow(gameModel);
+        
+        Point birdPosition = new Point((int)(mainWindow.getWidth() / 2), (int)(mainWindow.getHeight() / 2));
+        
+        MainMenuActionListener mainMenuActionListener = new MainMenuActionListener(mainWindow);
+        GameKeyAction gameKeyAction = new GameKeyAction(gameModel);
+
+        MainMenuPanel mainMenuPanel = mainWindow.getMainMenuPanel();
+        GamePanel gamePanel = mainWindow.getGamePanel();
+
+        gameModel.createBird(birdPosition, gamePanel.getFloorPosition());
+        gameModel.addObserver(gamePanel);
+
+        mainMenuPanel.registerListeners(mainMenuActionListener);
+        gamePanel.registerListeners(gameKeyAction);
+
+        gameModel.startGravity();
     }
 }
