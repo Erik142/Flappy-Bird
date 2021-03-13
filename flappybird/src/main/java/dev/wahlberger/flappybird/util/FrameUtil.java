94498eed33ff81f850a63b4f3bc0ec638c63d320
Erik142 <EWahlberger@gmail.com>
2021-03-13 08:35:38 +0100

    Add FrameUtil class

---
+++ flappybird/src/main/java/dev/wahlberger/flappybird/util/FrameUtil.java
@@ -0,0 +1,17 @@
+package dev.wahlberger.flappybird.util;
+
+import java.awt.Toolkit;
+import java.awt.Dimension;
+import javax.swing.JFrame;
+
+public class FrameUtil {
+   public static void centerFrame(JFrame frame) {
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+		Dimension windowSize = frame.getSize();
+		
+		int windowXPos = (int)(screenSize.getWidth() / 2 - windowSize.getWidth() / 2);
+		int windowYPos = (int)(screenSize.getHeight() / 2 - windowSize.getHeight() / 2);
+		
+		frame.setBounds(windowXPos, windowYPos, (int)windowSize.getWidth(), (int)windowSize.getHeight());
+   } 
+}
