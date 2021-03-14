947782f2a958e964123f5c832bc0cf39f4757a6b
Erik142 <EWahlberger@gmail.com>
2021-03-14 12:46:10 +0100

    Update everything on same thread

--- flappybird/src/main/java/dev/wahlberger/flappybird/model/GameModel.java
+++ flappybird/src/main/java/dev/wahlberger/flappybird/model/GameModel.java
@@ -24,8 +24,7 @@ public class GameModel extends AbstractObservable<GameModel> {
     private BirdSprite bird = null;
     private final PipePairSprite[] pairSprites;
 
-    private Thread fallThread;
-    private Thread movePipeThread;
+    private Thread gameThread;
 
     private PipePairSprite lastPipe;
     private PipePairSprite nextPipe;
@@ -57,8 +56,53 @@ public class GameModel extends AbstractObservable<GameModel> {
 
     public void createBird(Point initialPosition, double floorPosition) throws URISyntaxException, IOException {
         this.bird = new BirdSprite(initialPosition, floorPosition);
+    }
 
-        this.fallThread = new Thread(() -> {
+    public void createScoreboard(int windowWidth) throws URISyntaxException, IOException {
+        scoreBoard = new Scoreboard(windowWidth, SCOREBOARD_HEIGHT);
+    }
+    
+    public void createGameOverScreen(int windowWidth) throws URISyntaxException, IOException {
+        gameOverSprite = new GameOverSprite(new Point(windowWidth/2, GAMEOVER_HEIGHT));
+    }
+
+    public void createPipes(int floorPosition, int windowWidth) throws URISyntaxException, IOException {
+        for (int i = 0; i < pairSprites.length; i++) {
+            PipePairSprite sprite = new PipePairSprite(floorPosition, windowWidth + 300 + i*GAP_BETWEEN_PIPES);
+            this.pairSprites[i] = sprite;
+        }
+
+        lastPipe = this.pairSprites[NUM_OF_PIPES - 1];
+        nextPipe = this.pairSprites[0];
+
+        updateObservers(this);
+    }
+
+    public void performJump() {
+        if (!bird.isCrashed()) {
+            bird.performJump();
+            new Thread(() -> {
+                playSound("wing.wav");
+            }).start();
+        }
+
+        updateObservers(this);
+    }
+
+    private synchronized void playSound(String path) {
+        try {
+            Clip clip = AudioSystem.getClip();
+            AudioInputStream inputStream = AudioSystem.getAudioInputStream(
+            new BufferedInputStream(getClass().getClassLoader().getResourceAsStream(path)));
+            clip.open(inputStream);
+            clip.start(); 
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void startGame() {
+        this.gameThread = new Thread(() -> {
             boolean playedSound = false;
             int currentUpdate = 0;
             while (bird.isGravityEnabled()) {
@@ -80,10 +124,32 @@ public class GameModel extends AbstractObservable<GameModel> {
                     }
                 }
 
+                for (int i = 0; i < this.pairSprites.length; i++) {
+                    PipePairSprite sprite = pairSprites[i];
+                    sprite.movePipes();
+    
+                    if (sprite.equals(nextPipe) && (sprite.getXPosition() + sprite.getTopPipe().getImage().getWidth()) < bird.getPosition().x) {
+                        nextPipe = pairSprites[(i + 1) % NUM_OF_PIPES];
+                        
+                        try {
+                            scoreBoard.increaseScore();
+                            playSound("point.wav");
+                        } catch (URISyntaxException | IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+    
+                    if (sprite.isOutsideOfScreen()) {
+                        sprite.setXPosition(lastPipe.getXPosition() + GAP_BETWEEN_PIPES);
+                        sprite.generatePositions();
+                        lastPipe = sprite;
+                    }
+                }
+
                 updateObservers(this);
                 
                 try {
-                    Thread.sleep(10);
+                    Thread.sleep(9);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
@@ -98,116 +164,25 @@ public class GameModel extends AbstractObservable<GameModel> {
                 }
             }
         });
-    }
 
-    public void createScoreboard(int windowWidth) throws URISyntaxException, IOException {
-        scoreBoard = new Scoreboard(windowWidth, SCOREBOARD_HEIGHT);
-    }
-    
-    public void createGameOverScreen(int windowWidth) throws URISyntaxException, IOException {
-        gameOverSprite = new GameOverSprite(new Point(windowWidth/2, GAMEOVER_HEIGHT));
-    }
+        for (PipePairSprite sprite : pairSprites) {
+            sprite.startMove();
+        } 
 
-    public void createPipes(int floorPosition, int windowWidth) throws URISyntaxException, IOException {
-        for (int i = 0; i < pairSprites.length; i++) {
-            PipePairSprite sprite = new PipePairSprite(floorPosition, windowWidth + 300 + i*GAP_BETWEEN_PIPES);
-            this.pairSprites[i] = sprite;
-        }
-
-        lastPipe = this.pairSprites[NUM_OF_PIPES - 1];
-        nextPipe = this.pairSprites[0];
-
-        updateObservers(this);
-
-        this.movePipeThread = new Thread(() -> {
-            while (!bird.isCrashed()) {
-                for (int i = 0; i < this.pairSprites.length; i++) {
-                    PipePairSprite sprite = pairSprites[i];
-                    sprite.movePipes();
-
-                    if (sprite.equals(nextPipe) && (sprite.getXPosition() + sprite.getTopPipe().getImage().getWidth()) < bird.getPosition().x) {
-                        nextPipe = pairSprites[(i + 1) % NUM_OF_PIPES];
-                        
-                        try {
-                            scoreBoard.increaseScore();
-                            playSound("point.wav");
-                        } catch (URISyntaxException | IOException e) {
-                            e.printStackTrace();
-                        }
-                    }
-
-                    if (sprite.isOutsideOfScreen()) {
-                        sprite.setXPosition(lastPipe.getXPosition() + GAP_BETWEEN_PIPES);
-                        sprite.generatePositions();
-                        lastPipe = sprite;
-                    }
-                }
-                
-                updateObservers(this);
-                
-                try {
-                    Thread.sleep(10);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-        });
-    }
-
-    public void performJump() {
-        if (!bird.isCrashed()) {
-            bird.performJump();
-            new Thread(() -> {
-                playSound("wing.wav");
-            }).start();
-        }
-        updateObservers(this);
-    }
-
-    private synchronized void playSound(String path) {
-        try {
-            Clip clip = AudioSystem.getClip();
-            AudioInputStream inputStream = AudioSystem.getAudioInputStream(
-            new BufferedInputStream(getClass().getClassLoader().getResourceAsStream(path)));
-            clip.open(inputStream);
-            clip.start(); 
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    public void startGame() {
-        if (movePipeThread != null) {
-           for (PipePairSprite sprite : pairSprites) {
-               sprite.startMove();
-           } 
-
-           movePipeThread.start();
-        }
-
-        if (fallThread != null) {
-            bird.startUpdatePosition();
-            fallThread.start();
-        }
+        bird.startUpdatePosition();
+        gameThread.start();
     }
 
     public void stopGame() {
-        if (movePipeThread != null) {
+        if (gameThread != null) {
             for (PipePairSprite sprite : pairSprites) {
                 sprite.stopMove();
             }
 
-            try {
-                movePipeThread.join();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
+            bird.stopUpdatePosition();
 
-        if (fallThread != null) {
             try {
-                bird.stopUpdatePosition();
-                fallThread.join();
+               gameThread.join();
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
